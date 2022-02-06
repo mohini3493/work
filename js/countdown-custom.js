832bc8588ce2b07026931ee2ffb385293e30f79a
Mohiniba Jadeja <mohini3493@gmail.com>
2022-02-06 15:06:38 +0530

    Delete js directory

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2020, 8, 10, 8)}); // year, month, date, hour
-        });
-});		
-
